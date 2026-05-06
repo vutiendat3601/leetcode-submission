8369d0c96a1ac569e07c7f3ce34ae74b05c24e6b
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-06 14:37:37 +0700

    Time: 16 ms (11.87%), Space: 14.5 MB (7.94%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -3,37 +3,47 @@ private:
     string s, p;
     int m, n;
 
-    bool backtrack(int i, int j) {
+    bool backtrack(int i, int j, unordered_map<string, bool>& cache) {
         if (j >= n)
             return i >= m;
-
-        if ((j + 1 < n ? p[j + 1] : '\0') == '*') {
-            int k = i;
-            if (p[j] == '.') {
-                while (k <= m) {
-                    if (backtrack(k, j + 2))
-                        return 1;
-                    k++;
+        string key = to_string(i) + "," + to_string(j);
+        if (!cache.count(key)) {
+            cache[key] = 0;
+            if ((j + 1 < n ? p[j + 1] : '\0') == '*') {
+                int k = i;
+                if (p[j] == '.') {
+                    while (k <= m) {
+                        if (backtrack(k, j + 2, cache)) {
+                            cache[key] = 1;
+                            break;
+                        }
+                        k++;
+                    }
+                } else {
+                    if (backtrack(i, j + 2, cache)) {
+                        cache[key] = 1;
+                    } else {
+                        while (k < m && s[k] == p[j]) {
+                            if (backtrack(k + 1, j + 2, cache)) {
+                                cache[key] = 1;
+                                break;
+                            }
+                            k++;
+                        }
+                    }
                 }
             } else {
-                if (backtrack(i, j + 2))
-                    return 1;
-                while (k < m && s[k] == p[j]) {
-                    if (backtrack(k + 1, j + 2))
-                        return 1;
-                    k++;
-                }
+                if (i < m && (p[j] == s[i] || p[j] == '.'))
+                    cache[key] = backtrack(i + 1, j + 1, cache);
             }
-        } else {
-            if (i < m && (p[j] == s[i] || p[j] == '.'))
-                return backtrack(i + 1, j + 1);
         }
-        return 0;
+        return cache[key];
     }
 
 public:
     bool isMatch(string s, string p) {
         m = s.size(), n = p.size(), this->s = s, this->p = p;
-        return backtrack(0, 0);
+        unordered_map<string, bool> cache;
+        return backtrack(0, 0, cache);
     }
 };
