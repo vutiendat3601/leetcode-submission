c49a78851e069671a69af275ccd9c8cfd9830746
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-06 15:24:45 +0700

    Time: 0 ms (100%), Space: 22.4 MB (99.98%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size(), last = INT_MIN, i = 0, j = 0, ans = 0;
+
+        while (j < n) {
+            if (last != nums[j])
+                nums[i++] = nums[j], ans++;
+            last = nums[j], j++;
+        }
+
+        return ans;
+    }
+};
+
+// 2-pointers, time: O(n), space: O(1)
