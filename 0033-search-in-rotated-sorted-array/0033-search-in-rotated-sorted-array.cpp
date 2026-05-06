cad7910a185747c98cb49d7debe29b1e3a2723de
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-06 15:37:46 +0700

    Time: 0 ms (100%), Space: 15.2 MB (66.67%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size(), l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < nums[0]) {
+                if (target > nums[n - 1] || target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+
+            } else {
+                if (target < nums[0] || target > nums[mid])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+// Binary Search, time: O(log(n)), space: O(1)
