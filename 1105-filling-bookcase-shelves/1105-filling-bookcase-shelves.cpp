ae447cda57b0d5bb980ace338993bda91a7b0ced
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-07 10:47:08 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,29 +1,21 @@
 class Solution {
-private:
-    int shelfWidth, n;
-
-    int dfs(vector<vector<int>>& books, int k, unordered_map<int, int>& cache) {
-        if (k >= n)
-            return 0;
-        if (!cache.count(k)) {
-            int cur_width = 0, mx_height = 0, i = k;
-            cache[k] = INT_MAX;
-            while (i < n && cur_width + books[i][0] <= shelfWidth) {
-                cur_width += books[i][0],
-                    mx_height = max(mx_height, books[i][1]);
-                cache[k] = min(cache[k], mx_height + dfs(books, i + 1, cache));
-                i++;
-            }
-        }
-        return cache[k];
-    }
-
 public:
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        n = books.size(), this->shelfWidth = shelfWidth;
-        unordered_map<int, int> cache;
-        return dfs(books, 0, cache);
+        int n = books.size();
+        vector<int> dp(n + 1, INT_MAX);
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = books[i][1] + dp[i + 1];
+            int j = i + 1, curWidth = books[i][0], curHeight = books[i][1];
+            while (j < n && curWidth + books[j][0] <= shelfWidth) {
+                curWidth += books[j][0],
+                    curHeight = max(curHeight, books[j][1]);
+                dp[i] = min(dp[i], curHeight + dp[j + 1]);
+                j++;
+            }
+        }
+        return dp[0];
     }
 };
 
-// DFS + Memorization, time: O(n), space: O(n)
+// DP, time: O(n), space: O(n)
