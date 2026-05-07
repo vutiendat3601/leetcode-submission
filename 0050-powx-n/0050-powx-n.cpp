0594b42da27158df87a0e8bb951233ee6a38a575
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-07 09:47:15 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (!n || n == 1)
+            return n ? x : 1;
+
+        double tmp = 0;
+        if (n > 1) {
+            tmp = myPow(x, n / 2), tmp *= tmp;
+            if ((n & 1) > 0)
+                tmp *= x;
+        } else if (n < 0) {
+            tmp = myPow(x, -(n / 2)), tmp *= tmp;
+            if ((-n & 1) > 0)
+                tmp *= x;
+            tmp = 1.0 / tmp;
+        }
+        return tmp;
+    }
+};
