fc955c0b9775119504229b8d613636ead6eb8a52
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-07 15:20:09 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,21 +1,14 @@
 class Solution {
-private:
-    int dfs(vector<int>& nums, int target, unordered_map<int, int>& cache) {
-        if (!target)
-            return 1;
-        if (!cache.count(target)) {
-            cache[target] = 0;
-            for (auto& num : nums)
-                if (target - num >= 0)
-                    cache[target] += dfs(nums, target - num, cache);
-        }
-        return cache[target];
-    }
-
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        unordered_map<int, int> cache;
-        return dfs(nums, target, cache);
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (auto& num : nums)
+                if (i - num >= 0)
+                    dp[i] += dp[i - num];
+        }
+        return dp[target];
     }
 };
 
