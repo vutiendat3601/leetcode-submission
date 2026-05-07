98824f2845d06eb84db9b54933ddf0f95bfdeca0
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-07 09:52:06 +0700

    Time: 0 ms (100%), Space: 9.1 MB (98.01%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int m = matrix.size(), n = matrix[0].size();
+        int left = 0, right = n, top = 0, bottom = m;
+        while (left < right && top < bottom) {
+            for (int i = left; i < right; i++)
+                ans.push_back(matrix[top][i]);
+            top++;
+            for (int i = top; i < bottom; i++)
+                ans.push_back(matrix[i][right - 1]);
+            right--;
+
+            if (left >= right || top >= bottom)
+                break;
+
+            for (int i = right - 1; i >= left; i--)
+                ans.push_back(matrix[bottom - 1][i]);
+            bottom--;
+            for (int i = bottom - 1; i >= top; i--)
+                ans.push_back(matrix[i][left]);
+            left++;
+        }
+        return ans;
+    }
+};
