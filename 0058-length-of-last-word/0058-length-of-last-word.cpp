ec822b3d73ae98eb9026c9c72fe124b0589f11c0
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-07 09:57:05 +0700

    Time: 0 ms (100%), Space: 8.8 MB (67.01%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size(), ans = 0, i = n - 1;
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        while (i >= 0 && s[i] != ' ')
+            ans++, i--;
+        return ans;
+    }
+};
