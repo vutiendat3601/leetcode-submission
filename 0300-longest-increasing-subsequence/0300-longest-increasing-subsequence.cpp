f288ef992dca98f5618b256d43385b984fdf6f49
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-08 10:46:49 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,28 +1,16 @@
 class Solution {
-private:
-    int n;
-    int dfs(vector<int>& nums, int k, vector<int>& cache) {
-        if (k >= n)
-            return 0;
-        if (!cache[k]) {
-            cache[k] = 1;
-            for (int i = k + 1; i < n; i++)
-                if (nums[i] > nums[k])
-                    cache[k] = max(cache[k], 1 + dfs(nums, i, cache));
-        }
-
-        return cache[k];
-    }
-
 public:
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
-        vector<int> cache(n, 0);
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-            ans = max(ans, dfs(nums, i, cache));
+        int n = nums.size(), ans = 0;
+        vector<int> dp(n, 1);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++)
+                if (nums[j] > nums[i])
+                    dp[i] = max(dp[i], 1 + dp[j]);
+            ans = max(ans, dp[i]);
+        }
         return ans;
     }
 };
 
-// DFS + Memorization, time: O(n), space: O(n)
+// DP, time: O(n.log(n)), space: O(n)
