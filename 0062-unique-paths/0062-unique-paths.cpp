aabd5cfd1d29813b019a6621b7a94160987dc16b
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-15 14:20:45 +0700

    Time: 0 ms (100%), Space: 9.6 MB (27.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,21 @@
 class Solution {
+private:
+    int dfs(int i, int j, int m, int n, vector<vector<int>>& cache) {
+        if (i >= m || j >= n)
+            return 0;
+        if (i == m - 1 && j == n - 1)
+            return 1;
+        if (!cache[i][j])
+            cache[i][j] =
+                dfs(i + 1, j, m, n, cache) + dfs(i, j + 1, m, n, cache);
+        return cache[i][j];
+    }
+
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; i++)
-            for (int j = 1; j < n; j++)
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        return dp[m - 1][n - 1];
+        vector<vector<int>> cache(m, vector<int>(n, 0));
+        return dfs(0, 0, m, n, cache);
     }
 };
 
-// DP, time: O(m.n), space: O(m.n)
+// Brute Force, time: O(2^(m.n)), space: O(2^(m.n))
