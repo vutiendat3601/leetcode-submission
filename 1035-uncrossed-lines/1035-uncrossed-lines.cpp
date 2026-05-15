21da3689b6f6c00a92497c1ad55aae01e21c3642
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-15 16:33:22 +0700

    Time: 4 ms (76.92%), Space: 16.7 MB (58.34%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,27 +1,16 @@
 class Solution {
-private:
-    int m, n;
-    int dfs(vector<int>& nums1, vector<int>& nums2, int i, int j,
-            unordered_map<string, int>& cache) {
-        if (i >= m)
-            return 0;
-        string key = to_string(i) + "," + to_string(j);
-        if (!cache.count(key)) {
-            cache[key] = dfs(nums1, nums2, i + 1, j, cache);
-            for (int k = j; k < n; k++)
-                if (nums2[k] == nums1[i])
-                    cache[key] = max(
-                        cache[key], 1 + dfs(nums1, nums2, i + 1, k + 1, cache));
-        }
-        return cache[key];
-    }
-
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        m = nums1.size(), n = nums2.size();
-        unordered_map<string, int> cache;
-        return dfs(nums1, nums2, 0, 0, cache);
+        int m = nums1.size(), n = nums2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int r = 1; r <= m; r++)
+            for (int c = 1; c <= n; c++) {
+                int d = nums1[r - 1] == nums2[c - 1] ? 1 : 0;
+                dp[r][c] =
+                    max(max(dp[r - 1][c], dp[r][c - 1]), dp[r - 1][c - 1] + d);
+            }
+        return dp[m][n];
     }
 };
 
-// Brute Force, time: O(m^n), space: O(m^n)
+// DP, time: O(m.n), space: O(m.n)
