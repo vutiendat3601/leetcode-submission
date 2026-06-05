54dac32b3b525a11f5999d1c9d00656168f716e8
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-06-05 10:36:50 +0700

    Time: 0 ms (100%), Space: 12.8 MB (21%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<vector<int>> ans;
+    vector<int> nums, ans_item;
+    int n = 0;
+
+    void backtrack(int i) {
+        if (i >= n) {
+            ans.push_back(ans_item);
+            return;
+        }
+        backtrack(i + 1);
+        ans_item.push_back(nums[i]);
+        backtrack(i + 1);
+        ans_item.pop_back();
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        this->nums = nums;
+        n = nums.size();
+        backtrack(0);
+        return ans;
+    }
+};
