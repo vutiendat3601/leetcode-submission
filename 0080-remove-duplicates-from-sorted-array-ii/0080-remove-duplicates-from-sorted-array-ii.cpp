77c44067ec1af375eedf411d854da1e0e0ebe3d1
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-06-06 14:54:12 +0700

    Time: 9 ms (49.63%), Space: 19.6 MB (23.57%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,14 +3,12 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int ans = 0, n = nums.size(), j = 0, last = INT_MIN, cnt = 0;
         for (int i = 0; i < n; i++) {
-            if (nums[i] == last) {
+            if (nums[i] == last)
                 cnt++;
-            } else {
+            else
                 cnt = 1;
-            }
-            if (cnt < 3) {
+            if (cnt < 3)
                 nums[j++] = nums[i];
-            }
             last = nums[i];
         }
         return j;
