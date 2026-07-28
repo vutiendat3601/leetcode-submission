d8d5b0b2e1ab8ecd2f9b03323a2dddf626aac081
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-07-28 16:42:16 +0700

    Time: 128 ms (19.78%), Space: 62.6 MB (73.85%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        unordered_map<char, int> cnts;
+        for (auto& c : s)
+            cnts[c]++;
+        int n = s.size(), l = 0, r = n - 1;
+        string ans(n, '1');
+        char last = '1';
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (cnts[c] > 1) {
+                ans[l] = ans[r] = c;
+                l++, r--;
+                cnts[c] -= 2;
+            }
+            if (cnts[c])
+                last = c;
+        }
+        if (n & 1)
+            ans[l] = last;
+        return ans;
+    }
+};
