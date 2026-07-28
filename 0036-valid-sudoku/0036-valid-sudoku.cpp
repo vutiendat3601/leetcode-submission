4d7aefd2061ff4f4c6745560be1bdb36dea07833
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-07-28 16:32:36 +0700

    Time: 14 ms (6.26%), Space: 25.3 MB (28.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,33 +1,21 @@
 class Solution {
-private:
-    const int N = 9;
-
 public:
-    bool isValidSudoku(vector<vector<char>>& b) {
-        vector<unordered_set<char>> cols(N), rows(N), area(N);
+    bool isValidSudoku(vector<vector<char>>& board) {
+        const int N = board.size();
+        vector<unordered_set<char>> rows(9), cols(9), squares(9);
+
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                if (isdigit(b[r][c])) {
-                    // check column
-                    if (cols[c].count(b[r][c]))
-                        return 0;
-
-                    // Check row
-                    if (rows[r].count(b[r][c]))
-                        return 0;
-
-                    // Check area box
-                    int k = (r / 3) * 3 + c / 3;
-                    if (area[k].count(b[r][c]))
-                        return 0;
-
-                    rows[r].insert(b[r][c]), cols[c].insert(b[r][c]),
-                        area[k].insert(b[r][c]);
-                }
+                char z = board[r][c];
+                if (z == '.')
+                    continue;
+                int y = r / 3 * 3 + c / 3;
+                if (rows[r].count(z) || cols[c].count(z) || squares[y].count(z))
+                    return 0;
+                rows[r].insert(z), cols[c].insert(z), squares[y].insert(z);
             }
         }
+
         return 1;
     }
 };
-
-// Hashing, time: O(n^2), space: O(n^3)
