3bc6151023ac3bb4123461cfd3d90794ef778418
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-01 20:27:25 +0700

    Time: 0 ms (100%), Space: 10.7 MB (24.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,14 @@
 class Solution {
-private:
-    int dfs(int k, vector<int>& nums, vector<int>& cache) {
-        int n = nums.size();
-        if (k >= n)
-            return 0;
-        if (cache[k] < 0)
-            cache[k] =
-                max(nums[k] + dfs(k + 2, nums, cache), dfs(k + 1, nums, cache));
-        return cache[k];
-    }
-
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> cache(n, -1);
-        return dfs(0, nums, cache);
+        if (n == 1)
+            return nums[0];
+        vector<int> dp(n + 1, 0);
+        dp[1] = nums[0];
+        for (int k = 2; k <= n; k++)
+            dp[k] = max(nums[k - 1] + dp[k - 2], dp[k - 1]);
+
+        return dp[n];
     }
 };
