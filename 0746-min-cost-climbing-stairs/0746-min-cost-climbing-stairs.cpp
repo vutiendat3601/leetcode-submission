87237928eb0ebe80d8303f76b55c017e1e1dcfae
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-01 20:16:54 +0700

    Time: 0 ms (100%), Space: 17.5 MB (74.93%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,10 @@
 class Solution {
-private:
-    int dfs(int k, vector<int>& cost, vector<int>& cache) {
-        if (!k || k == 1)
-            return 0;
-        if (cache[k] < 0)
-            cache[k] = min(cost[k - 2] + dfs(k - 2, cost, cache),
-                           cost[k - 1] + dfs(k - 1, cost, cache));
-        return cache[k];
-    }
-
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> cache(n + 1, -1);
-        return dfs(n, cost, cache);
+        vector<int> dp(n + 1, 0);
+        for (int i = 2; i <= n; i++)
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        return dp[n];
     }
 };
