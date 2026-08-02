a3d7b5ffc9fa7ffdad9764f07254e0a82e91049e
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-02 15:31:04 +0700

    Time: 0 ms (100%), Space: 10.8 MB (37.99%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
-private:
-    int dfs(int k, int n, vector<int>& nums, vector<int>& cache) {
-        if (k >= n)
-            return 0;
-        if (cache[k] < 0)
-            cache[k] = max(nums[k] + dfs(k + 2, n, nums, cache),
-                           dfs(k + 1, n, nums, cache));
-        return cache[k];
-    }
-
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(), ans = 0;
         if (n == 1)
             return nums[0];
-        vector<int> cache1(n, -1), cache2(n, -1);
-        return max(dfs(0, n - 1, nums, cache1), dfs(1, n, nums, cache2));
+        vector<int> dp1(n, 0), dp2(n, 0);
+        dp1[0] = nums[0], dp1[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n - 1; i++)
+            dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i]);
+        dp2[1] = nums[1];
+        for (int i = 2; i < n; i++)
+            dp2[i] = max(dp2[i - 1], dp2[i - 2] + nums[i]);
+        return max(dp1[n - 2], dp2[n - 1]);
     }
 };
