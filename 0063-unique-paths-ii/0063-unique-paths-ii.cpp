b08b40ef7e020ee1e45b4193616874de6df63161
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-09 17:00:37 +0700

    Time: 0 ms (100%), Space: 12 MB (32.34%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
+private:
+    int dfs(vector<vector<int>>& grid, int m, int n,
+            vector<vector<int>>& cache) {
+        if (!m && !n)
+            return 1;
+        if (cache[m][n] < 0) {
+            cache[m][n] = 0;
+            if (m && !grid[m - 1][n])
+                cache[m][n] += dfs(grid, m - 1, n, cache);
+            if (n && !grid[m][n - 1])
+                cache[m][n] += dfs(grid, m, n - 1, cache);
+        }
+        return cache[m][n];
+    }
+
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = grid[0][0] ? 0 : 1;
-        for (int r = 0; r < m; r++)
-            for (int c = 0; c < n; c++) {
-                if (!grid[r][c]) {
-                    bool rb = r - 1 >= 0, cb = c - 1 >= 0;
-                    if (rb && cb)
-                        dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
-                    else if (rb)
-                        dp[r][c] = dp[r - 1][c];
-                    else if (cb)
-                        dp[r][c] = dp[r][c - 1];
-                }
-            }
-        return dp[m - 1][n - 1];
+        vector<vector<int>> cache(m, vector<int>(n, -1));
+        return grid[0][0] || grid[m - 1][n - 1] ? 0 : dfs(grid, m - 1, n - 1, cache);
     }
 };
-
-// DP, time: O(m.n), space: O(m.n)
