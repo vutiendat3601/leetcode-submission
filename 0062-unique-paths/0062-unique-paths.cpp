cc5a9a8387b00166d50fb85f0c73b9079d0cb7f3
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-20 17:51:52 +0700

    Time: 0 ms (100%), Space: 9.4 MB (54.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,21 @@
 class Solution {
 private:
-    int dfs(int m, int n, vector<vector<int>>& cache) {
-        if (!m || !n)
+    int dfs(int r, int c, vector<vector<int>>& cache) {
+        if (!r || !c)
             return 1;
-        if (!cache[m][n]) {
-            if (m)
-                cache[m][n] += dfs(m - 1, n, cache);
-            if (n)
-                cache[m][n] += dfs(m, n - 1, cache);
+        if (cache[r][c] < 0) {
+            cache[r][c] = 0;
+            if (r)
+                cache[r][c] += dfs(r - 1, c, cache);
+            if (c)
+                cache[r][c] += dfs(r, c - 1, cache);
         }
-        return cache[m][n];
+        return cache[r][c];
     }
 
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> cache(m, vector<int>(n, 0));
+        vector<vector<int>> cache(m, vector<int>(n, -1));
         return dfs(m - 1, n - 1, cache);
     }
 };
