7ff650c9f4211bea38f0f90f4a7c1ce70003f72e
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-22 19:51:52 +0700

    Time: 47 ms (24.99%), Space: 41.8 MB (17.27%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,13 +3,14 @@ private:
     int dfs(int amount, int k, vector<int>& coins, vector<vector<int>>& cache) {
         if (!amount)
             return 1;
+        if (k < 0)
+            return 0;
         int n = coins.size();
         if (cache[amount][k] < 0) {
             cache[amount][k] = 0;
-            for (int i = 0; i < n; i++) {
-                if (amount >= coins[i] && (k >= n ? 1 : coins[i] <= coins[k]))
-                    cache[amount][k] += dfs(amount - coins[i], i, coins, cache);
-            }
+            if (amount >= coins[k])
+                cache[amount][k] += dfs(amount - coins[k], k, coins, cache);
+            cache[amount][k] += dfs(amount, k - 1, coins, cache);
         }
         return cache[amount][k];
     }
@@ -17,7 +18,8 @@ private:
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> cache(amount + 1, vector<int>(n + 1, -1));
-        return dfs(amount, n, coins, cache);
+        sort(coins.begin(), coins.end());
+        vector<vector<int>> cache(amount + 1, vector<int>(n, -1));
+        return dfs(amount, n - 1, coins, cache);
     }
 };
