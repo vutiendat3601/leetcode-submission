3401a8539a47aef51260d72f98a57973d7f679c7
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-22 18:43:42 +0700

    Time: 13 ms (95.23%), Space: 17.7 MB (94.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,16 @@
 class Solution {
-private:
-    int dfs(int amount, vector<int>& coins, vector<int>& cache) {
-        if (!amount)
-            return 0;
-        if (cache[amount] == INT_MAX) {
-            bool available = 0;
-            for (auto& coin : coins) {
-                if (amount >= coin) {
-                    int ans = dfs(amount - coin, coins, cache);
-                    if (ans != -1)
-                        available = 1, cache[amount] = min(cache[amount], 1 + ans);
-                }
-            }
-            if (!available)
-                cache[amount] = -1;
-        }
-        return cache[amount];
-    }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> cache(amount + 1, INT_MAX);
-        return dfs(amount, coins, cache);
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (auto& coin : coins) {
+                if (i >= coin && dp[i - coin] != -1)
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+            }
+            if (dp[i] == INT_MAX)
+                dp[i] = -1;
+        }
+        return dp[amount];
     }
 };
