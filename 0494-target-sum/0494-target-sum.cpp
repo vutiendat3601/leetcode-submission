fb762ebd055ffac535092e566ad9afb16ba22496
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-23 21:46:58 +0700

    Time: 160 ms (31.74%), Space: 60.3 MB (7.9%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 private:
     int n, target;
-    int dfs(int k, int sum, vector<int>& nums) {
+    int dfs(int k, int sum, vector<int>& nums,
+            vector<unordered_map<int, int>>& cache) {
         if (k >= n)
             return sum == target ? 1 : 0;
-        return dfs(k + 1, sum + nums[k], nums) +
-               dfs(k + 1, sum - nums[k], nums);
+        if (!cache[k].count(sum))
+            cache[k][sum] = dfs(k + 1, sum + nums[k], nums, cache) +
+                            dfs(k + 1, sum - nums[k], nums, cache);
+        return cache[k][sum];
     }
 
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         n = nums.size(), this->target = target;
-        return dfs(0, 0, nums);
+        vector<unordered_map<int, int>> cache(n);
+        return dfs(0, 0, nums, cache);
     }
 };
