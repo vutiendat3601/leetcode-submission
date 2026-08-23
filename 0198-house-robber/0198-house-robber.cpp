345104149d99c152980cb479c600ab85ba355cd0
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-23 20:54:22 +0700

    Time: 0 ms (100%), Space: 10.7 MB (47.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,4 +1,5 @@
 class Solution {
+/*
 private:
     int n;
     int dfs(int k, vector<int>& nums, unordered_map<int, int>& cache) {
@@ -9,11 +10,15 @@ private:
                 max(nums[k] + dfs(k + 2, nums, cache), dfs(k + 1, nums, cache));
         return cache[k];
     }
+*/
 
 public:
     int rob(vector<int>& nums) {
-        n = nums.size();
-        unordered_map<int, int> cache;
-        return dfs(0, nums, cache);
+        int n = nums.size();
+        vector<int> dp(n + 1, 0);
+        dp[n - 1] = nums[n - 1];
+        for (int k = n - 2; k >= 0; k--)
+            dp[k] = max(nums[k] + dp[k + 2], dp[k + 1]);
+        return dp[0];
     }
 };
