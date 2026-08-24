9cf9afad113a74ba3dde2c09c5c1d224dbce570d
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-24 20:41:08 +0700

    Time: 4 ms (18.19%), Space: 16.2 MB (36.17%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+/*
     int n;
     int dfs(int k, int isSell, vector<int>& prices,
             vector<vector<int>>& cache) {
@@ -17,11 +18,20 @@ private:
         }
         return cache[k][isSell];
     }
-
+*/
 public:
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-        vector<vector<int>> cache(n, vector<int>(2, -1));
-        return dfs(0, 0, prices, cache);
+        int n = prices.size();
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        for (int k = n - 1; k >= 0; k--) {
+            for (int isSell = 0; isSell <= 1; isSell++) {
+                dp[k][isSell] = dp[k + 1][isSell];
+                if (isSell)
+                    dp[k][isSell] = max(dp[k][isSell], prices[k] + dp[k + 2][0]);
+                else
+                    dp[k][isSell] = max(dp[k][isSell], -prices[k] + dp[k + 1][1]);
+            }
+        }
+        return dp[0][0];
     }
 };
