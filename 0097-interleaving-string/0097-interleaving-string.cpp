bbefa6b922606f717ecb60a2f78cfc63f97265b2
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-25 11:33:55 +0700

    Time: 2 ms (64.4%), Space: 9.9 MB (33.33%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -2,28 +2,34 @@ class Solution {
 private:
     int n, n1, n2;
     string s, s1, s2;
-    bool dfs(int k, int i1, int i2, unordered_map<string, bool>& cache) {
+    bool dfs(int i1, int i2, vector<vector<int>>& cache) {
+        int k = i1 + i2;
         if (k >= n)
             return 1;
-        string key = to_string(k) + "_" + to_string(i1) + "_" + to_string(i2);
-        if (!cache.count(key)) {
+        if (i1 > n1 || i2 > n2)
+            return 0;
+        if (cache[i1][i2] < 0) {
             bool s1_check = i1 < n1 ? s[k] == s1[i1] : 0;
             bool s2_check = i2 < n2 ? s[k] == s2[i2] : 0;
-            if (s1_check)
-                cache[key] = dfs(k + 1, i1 + 1, i2, cache);
-            if (s2_check && !cache[key])
-                cache[key] = dfs(k + 1, i1, i2 + 1, cache);
+            cache[i1][i2] = 0;
+            if (s1_check && s2_check)
+                cache[i1][i2] =
+                    dfs(i1 + 1, i2, cache) || dfs(i1, i2 + 1, cache);
+            else if (s1_check)
+                cache[i1][i2] = dfs(i1 + 1, i2, cache);
+            else if (s2_check)
+                cache[i1][i2] = dfs(i1, i2 + 1, cache);
         }
-        return cache[key];
+        return cache[i1][i2];
     }
 
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        if (s1.size() + s2.size() != s3.size())
-            return 0;
         n = s3.size(), n1 = s1.size(), n2 = s2.size();
+        if (n1 + n2 != n)
+            return 0;
         this->s1 = s1, this->s2 = s2, s = s3;
-        unordered_map<string, bool> cache;
-        return dfs(0, 0, 0, cache);
+        vector<vector<int>> cache(n1 + 1, vector<int>(n2 + 1, -1));
+        return dfs(0, 0, cache);
     }
 };
