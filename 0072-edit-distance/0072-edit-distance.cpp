5f749161406b7dbd8f185b68b8725fd3de78b3a1
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-25 15:09:31 +0700

    Time: 11 ms (28.55%), Space: 13.1 MB (73.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,26 @@
 class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        int m = word1.length(), n = word2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = i;
-        for (int i = 0; i <= n; i++)
-            dp[0][i] = i;
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i - 1][j - 1];
-                if (word1[i - 1] != word2[j - 1])
-                    dp[i][j]++;
-                dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j]);
-                dp[i][j] = min(dp[i][j], 1 + dp[i][j - 1]);
-            }
+private:
+    int n1, n2;
+    int dfs(int k, int i, string& w1, string& w2, vector<vector<int>>& cache) {
+        if (k >= n1)
+            return n2 - i;
+        if (i >= n2)
+            return n1 - k;
+        if (cache[k][i] == INT_MAX) {
+            int match = 0;
+            cache[k][i] = min(1 + dfs(k + 1, i, w1, w2, cache),
+                              1 + (match = dfs(k + 1, i + 1, w1, w2, cache)));
+            cache[k][i] = min(cache[k][i], 1 + dfs(k, i + 1, w1, w2, cache));
+            if (w1[k] == w2[i])
+                cache[k][i] = min(cache[k][i], match);
         }
-        return dp[m][n];
+        return cache[k][i];
+    }
+
+public:
+    int minDistance(string w1, string w2) {
+        n1 = w1.size(), n2 = w2.size();
+        vector<vector<int>> cache(n1, vector<int>(n2, INT_MAX));
+        return dfs(0, 0, w1, w2, cache);
     }
 };
