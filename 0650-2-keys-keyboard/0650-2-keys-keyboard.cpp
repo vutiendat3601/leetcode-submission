074437a86ba9d1b59e8c83e30b6b0de9867203bd
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-25 16:57:55 +0700

    Time: 114 ms (5.04%), Space: 160.2 MB (7.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,26 +1,26 @@
 class Solution {
 private:
     int n;
-    int dfs(int a, int clipboard, unordered_map<string, int>& cache) {
+    int dfs(int a, int clipboard, vector<vector<int>>& cache) {
         if (a == n)
             return 0;
         if (a > n)
             return INT_MAX / 2;
         string key = to_string(a) + "_" + to_string(clipboard);
-        if (!cache.count(key)) {
-
-            cache[key] = 2 + dfs(a + a, a, cache);
+        if (cache[a][clipboard] == INT_MAX) {
+            cache[a][clipboard] = 2 + dfs(a + a, a, cache);
             if (clipboard)
-                cache[key] =
-                    min(cache[key], 1 + dfs(a + clipboard, clipboard, cache));
+                cache[a][clipboard] =
+                    min(cache[a][clipboard],
+                        1 + dfs(a + clipboard, clipboard, cache));
         }
-        return cache[key];
+        return cache[a][clipboard];
     }
 
 public:
     int minSteps(int n) {
         this->n = n;
-        unordered_map<string, int> cache;
+        vector<vector<int>> cache(n + 1, vector<int>(n + 1, INT_MAX));
         return dfs(1, 0, cache);
     }
 };
