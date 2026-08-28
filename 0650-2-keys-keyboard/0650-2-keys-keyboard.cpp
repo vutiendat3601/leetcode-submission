2f8931a6e67bff117c85bfb4dc245ff16674a73b
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-28 08:39:25 +0700

    Time: 60 ms (19.05%), Space: 159 MB (13.06%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,38 +1,30 @@
 class Solution {
-    /*
-    private:
-        int n;
-        int dfs(int a, int clipboard, vector<vector<int>>& cache) {
-            if (a == n)
-                return 0;
-            if (a > n)
-                return INT_MAX / 2;
-            string key = to_string(a) + "_" + to_string(clipboard);
-            if (cache[a][clipboard] == INT_MAX) {
-                cache[a][clipboard] = 2 + dfs(a + a, a, cache);
-                if (clipboard)
-                    cache[a][clipboard] =
-                        min(cache[a][clipboard],
-                            1 + dfs(a + clipboard, clipboard, cache));
-            }
-            return cache[a][clipboard];
+private:
+    const int MAX = 100000;
+    int n;
+    int dfs(int a, int clipboard, vector<vector<int>>& cache) {
+        if (a == n)
+            return 0;
+        if (a > n)
+            return MAX;
+
+        if (cache[a][clipboard] == INT_MAX) {
+            cache[a][clipboard] = MAX;
+            if (a + a <= n)
+                cache[a][clipboard] =
+                    min(cache[a][clipboard], 2 + dfs(a + a, a, cache));
+            if (clipboard && a + clipboard <= n)
+                cache[a][clipboard] =
+                    min(cache[a][clipboard],
+                        1 + dfs(a + clipboard, clipboard, cache));
         }
-    */
+        return cache[a][clipboard];
+    }
+
 public:
     int minSteps(int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, INT_MAX / 2));
-        int ans = INT_MAX;
-        dp[1][0] = 0;
-        for (int a = 1; a <= n; a++) {
-            for (int clipboard = 0; clipboard <= a; clipboard++) {
-                dp[a][clipboard] =
-                    min(dp[a][clipboard], 1 + dp[a - clipboard][clipboard]);
-                dp[a][a] = min(dp[a][a], dp[a][clipboard] + 1);
-            }
-        }
-        for (int clipboard = 0; clipboard <= n; clipboard++)
-            ans = min(ans, min(dp[n][clipboard], dp[n][n]));
-
-        return ans;
+        this->n = n;
+        vector<vector<int>> cache(n + 1, vector<int>(n + 1, INT_MAX));
+        return dfs(1, 0, cache);
     }
 };
