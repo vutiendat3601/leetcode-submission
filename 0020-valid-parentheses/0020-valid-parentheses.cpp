8cb4cc4414d3aa122021c7744a04243839c2480b
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-31 11:08:13 +0700

    Time: 310 ms (1.14%), Space: 8.8 MB (89.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,21 @@
+// Brute Force
+
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for (auto& c : s) {
-            if (c == '(' || c == '[' || c == '{') {
-                st.push(c);
-            } else {
-                if (!st.size()) {
-                    return 0;
-                }
-                if ((c == ')' && st.top() == '(') ||
-                    (c == ']' && st.top() == '[') ||
-                    (c == '}' && st.top() == '{')) {
-                    st.pop();
-                } else {
-                    return 0;
-                }
-            }
+        const string parentheses = "()", curly_brackets = "{}",
+                     square_brackets = "[]";
+        int pt, cb, sb;
+        bool found = 1;
+        while (found) {
+            found = 0;
+            if ((pt = s.find(parentheses)) != string::npos)
+                s.erase(pt, 2), found = 1;
+            if ((cb = s.find(curly_brackets)) != string::npos)
+                s.erase(cb, 2), found = 1;
+            if ((sb = s.find(square_brackets)) != string::npos)
+                s.erase(sb, 2), found = 1;
         }
-        return st.empty();
+        return !s.size();
     }
 };
-
-// Stack, time: O(n), space: O(n)
