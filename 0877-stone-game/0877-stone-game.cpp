3e9865a5b18d0c84153eac0580e51352e093e216
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-31 10:25:41 +0700

    Time: 8 ms (26.54%), Space: 19.7 MB (14.91%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,4 +1,36 @@
 class Solution {
+    /*
+    private:
+        int dfs(int l, int r, vector<int>& piles, vector<vector<int>>& cache) {
+            if (l > r)
+                return 0;
+            if (cache[l][r] < 0) {
+                int takeLeft = piles[l], takeRight = piles[r];
+                if ((r - l + 1) & 1)
+                    takeLeft = takeRight = 0;
+                cache[l][r] = max(takeLeft + dfs(l + 1, r, piles, cache),
+                                  takeRight + dfs(l, r - 1, piles, cache));
+            }
+            return cache[l][r];
+        }
+    */
 public:
-    bool stoneGame(vector<int>& piles) { return 1; }
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        int sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int l = n - 1; l >= 0; l--) {
+            for (int r = l; r < n; r++) {
+                int takeLeft = piles[l], takeRight = piles[r];
+                if ((r - l + 1) & 1)
+                    takeLeft = takeRight = 0;
+                if (l == r)
+                    dp[l][r] = takeLeft;
+                else
+                    dp[l][r] =
+                        max(takeLeft + dp[l + 1][r], takeRight + dp[l][r - 1]);
+            }
+        }
+        return dp[0][n - 1] > sum / 2;
+    }
 };
