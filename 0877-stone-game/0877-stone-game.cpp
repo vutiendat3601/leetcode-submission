af5459db0ececd5e2e2a5bcefdbcea15589c82bd
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-08-31 09:08:10 +0700

    Time: 15 ms (15.47%), Space: 19.7 MB (14.91%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,29 +1,23 @@
 class Solution {
 private:
-    int dfs(int st, int en, vector<int>& piles, vector<vector<int>>& cache) {
-        if (st > en)
+    int dfs(int l, int r, vector<int>& piles, vector<vector<int>>& cache) {
+        if (l > r)
             return 0;
-        if (cache[st][en] < 0) {
-            // Alice takes piles[st], Bob takes piles[en]
-            cache[st][en] = piles[st] + dfs(st + 1, en - 1, piles, cache);
-            // Alice takes piles[en], Bob takes piles[st]
-            cache[st][en] =
-                max(cache[st][en], piles[en] + dfs(st + 1, en - 1, piles, cache));
-            // Alice takes piles[st], Bob takes piles[st + 1]
-            cache[st][en] =
-                max(cache[st][en], piles[st] + dfs(st + 2, en, piles, cache));
-            // Alice takes piles[en], Bob takes piles[en - 1]
-            cache[st][en] =
-                max(cache[st][en], piles[en] + dfs(st, en - 2, piles, cache));
+        if (cache[l][r] < 0) {
+            int takeLeft = piles[l], takeRight = piles[r];
+            if ((r - l + 1) & 1)
+                takeLeft = takeRight = 0;
+            cache[l][r] = max(takeLeft + dfs(l + 1, r, piles, cache),
+                              takeRight + dfs(l, r - 1, piles, cache));
         }
-        return cache[st][en];
+        return cache[l][r];
     }
 
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size(), sum = accumulate(piles.begin(), piles.end(), 0);
-        vector<vector<int>> cache(n, vector<int>(n, -1));
-        int alice = dfs(0, n - 1, piles, cache);
-        return alice > sum / 2;
+        int n = piles.size();
+        int sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<int>> cache(n, vector<int>(n + 1, -1));
+        return dfs(0, n - 1, piles, cache) > sum / 2;
     }
 };
