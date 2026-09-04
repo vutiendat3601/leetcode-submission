f3d6c1e04bfb8f9fcd980f7b3d0d3abccb2f8aa1
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-09-04 15:18:48 +0700

    Time: 0 ms (100%), Space: 10 MB (22.68%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,51 +1,30 @@
 class Solution {
-    /*
-    private:
-        int n, n1, n2;
-        string s, s1, s2;
-        bool dfs(int i1, int i2, vector<vector<int>>& cache) {
-            int k = i1 + i2;
-            if (k >= n)
-                return 1;
-            if (i1 > n1 || i2 > n2)
-                return 0;
-            if (cache[i1][i2] < 0) {
-                bool s1_cont = i1 < n1 ? s[k] == s1[i1] : 0;
-                bool s2_cont = i2 < n2 ? s[k] == s2[i2] : 0;
-                cache[i1][i2] = 0;
-                if (s1_cont && s2_cont)
-                    cache[i1][i2] =
-                        dfs(i1 + 1, i2, cache) || dfs(i1, i2 + 1, cache);
-                else if (s1_cont)
-                    cache[i1][i2] = dfs(i1 + 1, i2, cache);
-                else if (s2_cont)
-                    cache[i1][i2] = dfs(i1, i2 + 1, cache);
-            }
-            return cache[i1][i2];
+private:
+    int m, n, p;
+    string s1, s2, s3;
+    bool dfs(int i, int j, vector<vector<int>>& cache) {
+        int k = i + j;
+        if (i >= m && j >= n)
+            return k == p;
+        if (cache[i][j] < 0) {
+            bool cont1 = i < m ? s1[i] == s3[k] : 0;
+            bool cont2 = j < n ? s2[j] == s3[k] : 0;
+            cache[i][j] = 0;
+            if (cont1)
+                cache[i][j] = dfs(i + 1, j, cache);
+            if (cont2)
+                cache[i][j] = cache[i][j] || dfs(i, j + 1, cache);
         }
-    */
+        return cache[i][j];
+    }
+
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s3.size(), n1 = s1.size(), n2 = s2.size();
-        if (n1 + n2 != n)
+        m = s1.size(), n = s2.size(), p = s3.size();
+        this->s1 = s1, this->s2 = s2, this->s3 = s3;
+        if (m + n != p)
             return 0;
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-        dp[n1][n2] = 1;
-        for (int i1 = n1; i1 >= 0; i1--) {
-            for (int i2 = n2; i2 >= 0; i2--) {
-                int k = i1 + i2;
-                if (k >= n)
-                    continue;
-                bool s1_matched = s3[k] == s1[i1],
-                     s2_matched = i2 < n2 ? s3[k] == s2[i2] : 0;
-                if (s1_matched && s2_matched)
-                    dp[i1][i2] = dp[i1 + 1][i2] || dp[i1][i2 + 1];
-                else if (s1_matched)
-                    dp[i1][i2] = dp[i1 + 1][i2];
-                else if (s2_matched)
-                    dp[i1][i2] = dp[i1][i2 + 1];
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>> cache(m + 1, vector<int>(n + 1, -1));
+        return dfs(0, 0, cache);
     }
 };
