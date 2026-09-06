8a1825b4a6fb3ec6567060c2d55e742adb3c82a6
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-09-06 15:36:41 +0700

    Time: 78 ms (6.56%), Space: 157.1 MB (21.76%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,27 +1,24 @@
 class Solution {
 private:
-    const int MAX = 100000;
+    const int MAX = 5000;
     int n;
-    int dfs(int a, int clipboard, vector<vector<int>>& cache) {
-        if (a == n)
+    int dfs(int k, int c, vector<vector<int>>& cache) {
+        if (k > n)
+            return MAX;
+        if (k == n)
             return 0;
-        if (cache[a][clipboard] == INT_MAX) {
-            cache[a][clipboard] = MAX;
-            if (a + a <= n)
-                cache[a][clipboard] =
-                    min(cache[a][clipboard], 2 + dfs(a + a, a, cache));
-            if (clipboard && a + clipboard <= n)
-                cache[a][clipboard] =
-                    min(cache[a][clipboard],
-                        1 + dfs(a + clipboard, clipboard, cache));
+        if (cache[k][c] < 0) {
+            if (c)
+                return min(1 + dfs(k + c, c, cache), 2 + dfs(k + k, k, cache));
+            return 2 + dfs(k + k, k, cache);
         }
-        return cache[a][clipboard];
+        return cache[k][c];
     }
 
 public:
     int minSteps(int n) {
         this->n = n;
-        vector<vector<int>> cache(n + 1, vector<int>(n + 1, INT_MAX));
+        vector<vector<int>> cache(n, vector<int>(n, -1));
         return dfs(1, 0, cache);
     }
 };
