761519943bf283258df6b9cf7c9f38d8138391ef
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-09-13 11:40:32 +0700

    Time: 27 ms (27.58%), Space: 9.8 MB (31.69%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,23 +1,17 @@
 class Solution {
 private:
     const int MAX = 5000;
-    int n;
-    int dfs(int k, int c, vector<vector<int>>& cache) {
-        if (k > n)
-            return MAX;
-        if (k == n)
-            return 0;
-        if (cache[k][c] < 0)
-            cache[k][c] = min(c ? 1 + dfs(k + c, c, cache) : MAX,
-                              2 + dfs(k + k, k, cache));
-
-        return cache[k][c];
-    }
 
 public:
     int minSteps(int n) {
-        this->n = n;
-        vector<vector<int>> cache(n, vector<int>(n, -1));
-        return dfs(1, 0, cache);
+        vector<int> dp(n + 1, 0);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = MAX;
+            for (int j = 1; j < i; j++) {
+                if (!(i % j))
+                    dp[i] = min(dp[i], dp[j] + i / j);
+            }
+        }
+        return dp[n];
     }
 };
